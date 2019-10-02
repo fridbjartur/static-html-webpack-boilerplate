d3a01bffb8ac67880159bbca96f57c9b9e5b2777
Fríðbjartur Miðberg <fm@Fribjarturs-MacBook-Pro.local>
2019-10-02 21:39:08 +0200

    Added Handlebars

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,59 +5,78 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const generateHTMLPlugins = () => glob.sync('./src/**/*.html', { ignore: './src/partials/**' }).map(
-  dir => new HTMLWebpackPlugin({
-    filename: path.basename(dir), // Output
-    template: dir, // Input
-    minify: {
-     removeComments: true,
-     collapseWhitespace: true,
-    },
-  }),
+	dir => new HTMLWebpackPlugin({
+		filename: path.basename(dir), // Output
+		template: dir, // Input
+		minify: {
+			removeComments: true,
+			collapseWhitespace: true,
+		},
+	}),
 );
 
 module.exports = {
-  node: {
-    fs: 'empty',
-  },
-  entry: ['./src/js/app.js', './src/style/main.scss'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-        options: {
-          interpolate: true,
-        },
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg|mp4)$/i,
-        loader: 'file-loader',
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
-    ],
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: './src/static/',
-        to: './static/',
-      },
-    ]),
-    ...generateHTMLPlugins(),
-  ],
-  stats: {
-    colors: true,
-  },
-  devtool: 'source-map',
+	node: {
+		fs: 'empty',
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.hbs'],
+	},
+	entry: ['./src/js/app.js', './src/style/main.scss'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'app.bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.hbs$/,
+				loader: 'handlebars-loader',
+				query: {
+					partialDirs: path.join(__dirname, 'src', 'partials'),
+				},
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader',
+				options: {
+					interpolate: true,
+				},
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|mp4)$/i,
+				loader: 'file-loader',
+			},
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+			},
+		],
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{
+				from: './src/static/',
+				to: './static/',
+			},
+		]),
+		// new HTMLWebpackPlugin({
+		//   template: 'src/index.hbs',
+		//   // templateParameters: require(`./file.json`)
+		// }),
+		...generateHTMLPlugins(),
+	],
+	stats: {
+		colors: true,
+	},
+	devtool: 'source-map',
 };
