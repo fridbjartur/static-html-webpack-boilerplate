802ce6e1106a245399cdd6a37fafb4e175b8586c
Fríðbjartur Miðberg <55000390+fridbjartur@users.noreply.github.com>
2019-10-02 13:53:25 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
 module.exports = {
-    "extends": "airbnb-base"
+	root: true,
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		quotes: ['error', 'single', { allowTemplateLiterals: true }],
+		semi: ['error', 'always'],
+		'comma-dangle': ['warn', 'always-multiline'],
+		'no-console': ['warn'],
+		'no-unused-vars': ['warn'],
+		'space-before-function-paren': ['error', {
+			'anonymous': 'never',
+			'named': 'never',
+			'asyncArrow': 'always'
+		}]
+	},
+	env: {
+		node: true,
+		es6: true,
+	},
+	extends: 'airbnb-base',
+	parser: 'babel-eslint',
 };
