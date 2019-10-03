1d16960c91aa4affcd86beb9eacc6bf54af475cc
Fridbjartur <fridbjartur@midberg.com>
2019-10-03 15:54:09 +0200

    Added JS presets

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -46,8 +46,13 @@ module.exports = {
 			},
 			{
 				test: /\.js$/,
-				use: 'babel-loader',
 				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
 			},
 			{
 				test: /\.hbs$/,
